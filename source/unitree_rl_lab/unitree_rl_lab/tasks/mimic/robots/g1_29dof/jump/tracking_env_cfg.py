e93fc1d6153ab60d0a3822672d72b820c7a8191c
xiaohu zhang <ustc-zxh@mail.ustc.edu.cn>
2026-05-22 18:18:32 +0800

    新增jump

---
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/mimic/robots/g1_29dof/jump/tracking_env_cfg.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+
+import os
+
+from isaaclab.utils import configclass
+
+import unitree_rl_lab.tasks.mimic.mdp as mdp
+from unitree_rl_lab.tasks.mimic.robots.g1_29dof.dance_102.tracking_env_cfg import (
+    VELOCITY_RANGE,
+    RobotEnvCfg as BaseRobotEnvCfg,
+)
+
+
+@configclass
+class CommandsCfg:
+    """Command specifications for the MDP."""
+
+    motion = mdp.MotionCommandCfg(
+        asset_name="robot",
+        motion_file=f"{os.path.dirname(__file__)}/jump.npz",
+        anchor_body_name="torso_link",
+        resampling_time_range=(1.0e9, 1.0e9),
+        debug_vis=True,
+        pose_range={
+            "x": (-0.05, 0.05),
+            "y": (-0.05, 0.05),
+            "z": (-0.01, 0.01),
+            "roll": (-0.1, 0.1),
+            "pitch": (-0.1, 0.1),
+            "yaw": (-0.2, 0.2),
+        },
+        velocity_range=VELOCITY_RANGE,
+        joint_position_range=(-0.1, 0.1),
+        body_names=[
+            "pelvis",
+            "left_hip_roll_link",
+            "left_knee_link",
+            "left_ankle_roll_link",
+            "right_hip_roll_link",
+            "right_knee_link",
+            "right_ankle_roll_link",
+            "torso_link",
+            "left_shoulder_roll_link",
+            "left_elbow_link",
+            "left_wrist_yaw_link",
+            "right_shoulder_roll_link",
+            "right_elbow_link",
+            "right_wrist_yaw_link",
+        ],
+    )
+
+
+@configclass
+class RobotEnvCfg(BaseRobotEnvCfg):
+    commands: CommandsCfg = CommandsCfg()
+
+
+class RobotPlayEnvCfg(RobotEnvCfg):
+    def __post_init__(self):
+        super().__post_init__()
+        self.scene.num_envs = 1
+        self.episode_length_s = 1e9
