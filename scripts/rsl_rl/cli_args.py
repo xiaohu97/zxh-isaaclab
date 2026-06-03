2013bdafb78d0342d30ce5eafd40432cdc2f409d
xiaohuzhang <ustc-zxh@mail.ustc.edu.cn>
2026-06-03 21:02:36 +0800

    1

--- scripts/rsl_rl/cli_args.py
+++ scripts/rsl_rl/cli_args.py
@@ -81,6 +81,8 @@ def update_rsl_rl_cfg(agent_cfg: RslRlOnPolicyRunnerCfg, args_cli: argparse.Name
         agent_cfg.load_run = args_cli.load_run
     if args_cli.checkpoint is not None:
         agent_cfg.load_checkpoint = args_cli.checkpoint
+    if args_cli.experiment_name is not None:
+        agent_cfg.experiment_name = args_cli.experiment_name
     if args_cli.run_name is not None:
         agent_cfg.run_name = args_cli.run_name
     if args_cli.logger is not None:
