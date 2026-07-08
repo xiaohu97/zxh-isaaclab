2ea460074e6675d99e9d4ae559fd5b6a33dd8228
xiaohu zhang <ustc-zxh@mail.ustc.edu.cn>
2026-07-08 23:33:13 +0800

    降低落地冲击速度再试一下

--- source/unitree_rl_lab/unitree_rl_lab/tasks/humanoid_ultra/base/humanoidultra27dof_env_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/humanoid_ultra/base/humanoidultra27dof_env_cfg.py
@@ -102,7 +102,7 @@ class Humanoidultra27dofRewardCfg(RewardCfg):
     )
     feet_impact_velocity = RewTerm(
         func=mdp.feet_impact_velocity,
-        weight=-1.5,
+        weight=-2.5,
         params={
             "sensor_cfg": SceneEntityCfg("contact_sensor", body_names=".*ankle_roll.*"),
             "asset_cfg": SceneEntityCfg("robot", body_names=".*_ankle_roll.*"),
